6e4eab27eff9af777a9560b0bf34d46753eb6443
bishop255 <andresalejandro2001@gmail.com>
2026-05-12 22:40:49 -0400

    chore: limpiar archivos movidos a Producto

--- models/Medicine.ts
+++
@@ -1,9 +0,0 @@
-export interface Medicine {
-    id?: String;
-    name: string;
-    dosage: string;
-    currentStock: number;
-    minStock: number;
-    category: 'Tableta' | 'Jarabe' | 'Inyección' | 'Otro' | string;
-    lastUpdated?: any; //Timestamp de Firebase
-}
